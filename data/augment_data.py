500bf35509f49e1c0d1b28d960ad64c643863be9
sherman0611 <sherman0611@gmail.com>
2024-04-06 19:11:27 +0100

    updated data augmentation file

--- data/augment_data.py
+++ data/augment_data.py
@@ -2,6 +2,6 @@ import pymesh
 
 def random_augment():
     augmented_mesh = pymesh.load_mesh('id0_0.obj')
-    pymesh.save_mesh('augmented_mesh', augmented_mesh)
+    pymesh.save_mesh('augmented_mesh.obj', augmented_mesh)
 
 random_augment()
