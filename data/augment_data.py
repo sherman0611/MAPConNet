bb4b2fdfd1b14452883f410e9276f243f38817b6
sherman0611 <sherman0611@gmail.com>
2024-04-06 19:39:11 +0100

    updated data augmentation file

--- data/augment_data.py
+++ data/augment_data.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pymesh
 
-def random_rotate():
-    original_mesh = pymesh.load_mesh('id0_0.obj')
-
+def random_rotate(mesh):
     # generate random rotation angles for each axis
     angles = np.random.uniform(low=0, high=2*np.pi, size=3)
 
@@ -14,10 +12,8 @@ def random_rotate():
     rotation_matrix[:3, :3] = np.dot(rotation_matrices[2], np.dot(rotation_matrices[1], rotation_matrices[0]))
 
     # apply rotation to vertices
-    rotated_vertices = np.dot(original_mesh.vertices, rotation_matrix[:3, :3].T)
+    rotated_vertices = np.dot(mesh.vertices, rotation_matrix[:3, :3].T)
 
-    rotated_mesh = pymesh.form_mesh(rotated_vertices, original_mesh.faces)
+    rotated_mesh = pymesh.form_mesh(rotated_vertices, mesh.faces)
 
     pymesh.save_mesh('augmented_mesh.obj', rotated_mesh)
-
-random_rotate()
