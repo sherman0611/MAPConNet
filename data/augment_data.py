86696b47f7c7690a0b8fbeefb0157f107a12cf62
sherman0611 <sherman0611@gmail.com>
2024-04-06 19:33:55 +0100

    updated data augmentation file

--- data/augment_data.py
+++ data/augment_data.py
@@ -1,7 +1,15 @@
+import numpy as np
 import pymesh
 
 def random_augment():
-    augmented_mesh = pymesh.load_mesh('id0_0.obj')
+    original_mesh = pymesh.load_mesh('id0_0.obj')
+
+    # generate random rotation angles for each axis
+    angles = np.random.uniform(low=0, high=2*np.pi, size=3)
+
+    # perform the rotation
+    augmented_mesh = pymesh.rotate(original_mesh, angles)
+
     pymesh.save_mesh('augmented_mesh.obj', augmented_mesh)
 
 random_augment()
